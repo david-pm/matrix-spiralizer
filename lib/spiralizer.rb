7c40a5f08bbd2b7923a4deb0f375d9f7c2ed3af2
davidpm <davidpm.io.web@gmail.com>
2017-07-02 20:25:35 -0700

    add spiralize method

--- lib/spiralizer.rb
+++ lib/spiralizer.rb
@@ -1,5 +1,31 @@
 require "spiralizer/version"
+require "pry"
 
 module Spiralizer
-  # Your code goes here...
+  def self.spiralize(matrix:)
+    str = String.new
+
+    until matrix.empty?
+      str += matrix.shift.join(" ") + " "
+      str += matrix.map(&:pop).join(" ") + " "           unless matrix.empty?
+      str += matrix.pop.reverse.join(" ") + " "          unless matrix.empty?
+      str += matrix.map(&:shift).reverse.join(" ") + " " unless matrix.empty?
+    end
+
+    # this works too...
+    #
+    # loop do
+    #   begin
+    #     str += matrix.shift.join(" ") + " "
+    #     str += matrix.map(&:pop).join(" ") + " "
+    #     str += matrix.pop.reverse.join(" ") + " "
+    #     str += matrix.map(&:shift).reverse.join(" ") + " "
+    #   rescue NoMethodError => e
+    #     break if [:reverse, :map, :join].include?(e.name)
+    #     fail e
+    #   end
+    # end
+
+    str.downcase.strip
+  end
 end
--- spec/spiralizer_spec.rb
+++ spec/spiralizer_spec.rb
@@ -1,11 +1,20 @@
 require "spec_helper"
 
 RSpec.describe Spiralizer do
+  context "Spiralize" do
+    it "returns a string in spiraling order" do
+      matrix = [['A','B','C','D'], ['E','F','G','H'], ['I','J','K','L'], ['M','N','O','P'], ['Q','R','S','T']]
+      expect(Spiralizer.spiralize(matrix: matrix)).to eq %Q{a b c d h l p t s r q m i e f g k o n j}
+    end
+
+    it "will handle numbers with multiple digits" do
+      matrix = [[1,2,3], [10, 11, 4], [9, 12, 5], [8, 7, 6]]
+      expect(Spiralizer.spiralize(matrix: matrix)).to eq %Q{1 2 3 4 5 6 7 8 9 10 11 12}
+    end
+  end
+
   it "has a version number" do
     expect(Spiralizer::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
