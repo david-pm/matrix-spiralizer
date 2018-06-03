4956749239c4b97281e5c76d8ec0bcbc5eba2990
david-pm <damcdonald@weedmaps.com>
2018-06-02 22:22:59 -0700

    refactor cli

--- lib/spiralizer/cli.rb
+++ lib/spiralizer/cli.rb
@@ -8,9 +8,11 @@ class Spiralizer::CLI < Thor
   desc 'go', 'starts a prompt that brings users to spiral heaven'
   def go
     clear_screen!
-    say "What would you like to do? (choose a number)\n 1 - Build a matrix\n 2 - Generate a random matrix"
-    @action = ask '> '
-    delegate_build_action
+    intro_prompt
+    build_matrix
+    output_matrix
+    action_prompt
+    delegate_action
   rescue Spiralizer::InvalidInput => e
     say "\nUh oh! #{e.message}"
   ensure
@@ -18,31 +20,35 @@ class Spiralizer::CLI < Thor
   end
 
   no_commands do
-    def delegate_build_action
+    def intro_prompt
+      say "What would you like to do? (choose a number)\n 1 - Build a matrix\n 2 - Generate a random matrix"
+      @action = ask '> '
+    end
+
+    def action_prompt
+      say "\nWhat would you like to do with it? (choose a number)\n 1 - Spiralize it\n 2 - Crisscross it"
+      @action = ask '> '
+    end
+
+    def build_matrix
       say "\n"
+
       case action
-      when '1'
-        range, dimensions = user_range_and_dimensions
-        @matrix = Spiralizer::Matrix.the_matrix(range: range, dimensions: dimensions)
-        last_steps
-      when '2'
-        range, dimensions = random_range_and_dimensions
-        @matrix = Spiralizer::Matrix.the_matrix(range: range, dimensions: dimensions)
-        last_steps
-      else
-        quit_softly
+      when '1' then range, dimensions = user_range_and_dimensions
+      when '2' then range, dimensions = random_range_and_dimensions
+      else quit_softly
       end
+
+      @matrix = Spiralizer::Matrix.the_matrix(range: range, dimensions: dimensions)
     end
 
     def delegate_action
       say "\n"
+
       case action
-      when '1'
-        say Spiralizer::Spiralize.new(matrix: matrix).perform
-      when '2'
-        say Spiralizer::Crisscross.new(matrix: matrix).perform
-      else
-        quit_softly
+      when '1' then say Spiralizer::Spiralize.new(matrix: matrix).perform
+      when '2' then say Spiralizer::Crisscross.new(matrix: matrix).perform
+      else quit_softly
       end
     end
 
@@ -55,18 +61,15 @@ class Spiralizer::CLI < Thor
 
     def random_range_and_dimensions
       say "\ngenerating..."
-      sleep 1
+      pause_for_effect
       range_response, dimensions = Spiralizer::MATRICES[rand(6)]
       range = range_response.split('-')
       return (range.first..range.last), dimensions
     end
 
-    def last_steps
+    def output_matrix
       say "\nHere is your M A T R I X\n------------------------"
-      matrix.each { |arr| say arr.join("\t") }
-      say "\nWhat would you like to do with it? (choose a number)\n 1 - Spiralize it\n 2 - Crisscross it"
-      @action = ask '> '
-      delegate_action
+      matrix.each { |inner| say inner.join("\t") }
     end
 
     def clear_screen!
@@ -74,10 +77,14 @@ class Spiralizer::CLI < Thor
       system 'clear'
     end
 
+    def pause_for_effect
+      sleep 0.5
+    end
+
     def quit_softly
       say "\nexiting..."
-      sleep 0.5
+      pause_for_effect
       exit!
     end
-  end # no_command block
+  end
 end
